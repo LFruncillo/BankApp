749d3dd33b681dceb8b4ffbca289d234db8c06e1
LFruncillo <luke.fruncillo@outlook.com>
2021-07-25 23:17:06 +0100

    removed db file

--- AppFiles/db_connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-    // Used to connect to database
-
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $db_name="bank";
-    $conn = mysqli_connect( $servername, $username, $password, $db_name ) or die('Not connected' . mysqli_connect_error());
-?>
